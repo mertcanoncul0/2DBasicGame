0863efee5ed43d96db634e70d3ffd5426ec8ec62
Mertcan Öncül <mertcanoncul@icloud.com>
2025-03-20 06:33:22 +0300

    Idle and Jump completed

--- Assets/Scripts/MoveController1.cs
+++ Assets/Scripts/MoveController1.cs
@@ -1,32 +1,48 @@
+using System.Data.SqlTypes;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float xInput;
+    private Animator anim;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    private float xInput;
 
     [Header("Collision Check")]
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask whatIsGround;
-    private bool isGrounded;
+    public bool isGrounded;
+
+    private bool facingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        AnimationControllers();
         CollisionChecks();
+        FlipController();
 
         xInput = Input.GetAxisRaw("Horizontal");
         Movement();
 
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
+
+    }
+
+    private void AnimationControllers()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void Movement()
@@ -34,15 +50,29 @@ public class MoveController : MonoBehaviour
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
     }
 
-    private void CollisionChecks()
+    private void FlipController()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (rb.linearVelocity.x < 0 && facingRight)
+            Flip();
+        else if (rb.linearVelocity.x > 0 && !facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 
     private void Jump()
     {
         if (isGrounded)
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     void OnDrawGizmos()
